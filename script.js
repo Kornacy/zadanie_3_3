eb87a79f4161129199019c5bf91e170c224ce8ce
Kamil <kamilbuszta.3@gmail.com>
2025-11-13 14:25:43 +0100

    C 3_3.1

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 (function () {
-  
+  const gifBtn = document.getElementById("gifBtn");
+  const answerDiv = document.getElementById("answer");
+  const apiKey = "yTC33gQaLNEOs2B2cShieHWxslQbIhte";
+  gifBtn.addEventListener("click", function() {
+    fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=&rating=g`)
+      .then(response => response.json())
+      .then(gifdata => {
+        const imgUrl = gifdata.data.images.original.url;
+        answerDiv.innerHTML = `<img src="${imgUrl}" alt="Random GIF">`;
+      })
+      .catch(error => {
+        console.error("Error fetching GIF:", error);
+        answerDiv.textContent = "Błąd ładowania GIF.";
+      });
+  });
+
 })();
