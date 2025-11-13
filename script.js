5ec44d6516923a1c13d3467ea76320abf47c7738
Kamil <kamilbuszta.3@gmail.com>
2025-11-13 14:50:19 +0100

    C 3_3.2

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
   const gifBtn = document.getElementById("gifBtn");
   const answerDiv = document.getElementById("answer");
   const apiKey = "yTC33gQaLNEOs2B2cShieHWxslQbIhte";
+  const searchBtn = document.getElementById("searchBtn");
+  const searchInput = document.getElementById("searchInput");
+
   gifBtn.addEventListener("click", function() {
     fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=&rating=g`)
       .then(response => response.json())
@@ -13,6 +16,34 @@
         console.error("Error fetching GIF:", error);
         answerDiv.textContent = "Błąd ładowania GIF.";
       });
+  });
+
+  searchBtn.addEventListener("click", function() {
+    var text = searchInput.value;
+    answerDiv.innerHTML = "";
+    var licz = 0;
+    var tableContent = "";
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(text)}&limit=9&offset=0&rating=g&lang=en`)
+      .then(response => response.json())
+      .then(gifdata => {
+        if (gifdata.data.length > 0) {
+          tableContent = "<table id='giftable'><tr>";
+          
+          gifdata.data.forEach(element => {
+            if (licz == 3) {
+              tableContent += "</tr><tr>";
+              licz = 0;
+            }
+            tableContent += `<td><img class="gif" src="${element.images.original.url}" alt="Search GIF"></td>`;
+            licz++;
+          });
+          tableContent += "</tr></table>";
+          answerDiv.innerHTML = tableContent;
+        } else {
+          answerDiv.innerHTML = "Brak wyników dla podanej frazy.";
+        }
+      });
+
   });
 
 })();
